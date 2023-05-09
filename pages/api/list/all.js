e6393a2f427e94e3356e89e7fb5133c5ca21adfc
marshmll <renandasilvaoliveiraandrade@gmail.com>
2023-05-09 16:30:24 -0300

    Adição de rota /all

--- pages/api/list/all.js
+++ pages/api/list/all.js
@@ -4,13 +4,7 @@ export default async function news(req, res) {
   const DATOCMS_TOKEN = process.env.DATOCMS_TOKEN;
   const client = buildClient({ apiToken: DATOCMS_TOKEN });
 
-  const type = req.query.type;
-
-  const records = await client.items.list({
-    filter: {
-      type: type,
-    },
-  });
+  const records = await client.items.list();
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
